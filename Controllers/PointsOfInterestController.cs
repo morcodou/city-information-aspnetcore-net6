9267cba95ddab363c56afc1f0f2266363a62a865
Mor Codou GUEYE <gueye.morcodou@rcgt.com>
2022-02-23 11:10:55 -0800

    Add point of interest controller

---
+++ Controllers/PointsOfInterestController.cs
@@ -0,0 +1,32 @@
+﻿using CityInformation.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CityInformation.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null) return NotFound();
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null) return NotFound();
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(x => x.Id == id);
+            if (pointOfInterest == null) return NotFound();
+            return Ok(pointOfInterest);
+        }
+    }
+}
--- Models/CityDto.cs
+++ Models/CityDto.cs
@@ -5,5 +5,15 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
+
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest?.Count ?? 0;
+            }
+        }
     }
 }
