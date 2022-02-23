ab336597e05d39960000917ab0770b75556097c0
Mor Codou GUEYE <gueye.morcodou@rcgt.com>
2022-02-23 12:57:48 -0800

    Add file controller

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInformation.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _contentTypeProvider;
+
+        public FilesController(FileExtensionContentTypeProvider contentTypeProvider)
+        {
+            _contentTypeProvider = contentTypeProvider;
+        }
+
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            var path = "file-download.pdf";
+            if (!System.IO.File.Exists(path))
+            {
+                return NotFound();
+            }
+
+
+            if(!_contentTypeProvider.TryGetContentType(path, out var contentType)){
+                contentType = "application/octet-stream";
+            }
+            var bytes  = System.IO.File.ReadAllBytes(path);
+            return File(bytes, contentType, Path.GetFileName(path));
+        }
+    }
+}
