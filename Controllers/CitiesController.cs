9c07ecd3b19c74ba33758953c9d0f9cc4e089f83
Mor Codou GUEYE <gueye.morcodou@rcgt.com>
2022-02-23 06:23:03 -0800

    Add initial cities controller

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -2,8 +2,20 @@
 
 namespace CityInformation.API.Controllers
 {
+    [ApiController]
+    [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
-
+        [HttpGet()]
+        public JsonResult Cities()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new { id = 1, name  = "name1"},
+                    new { id = 2, name  = "name2"},
+                }
+            );
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -18,8 +18,16 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+//app.MapControllers();
 
 app.Run();
