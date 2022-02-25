a9ed9bfbabf50c89c3490467f3a0072f45445080
Mor Codou GUEYE <gueye.morcodou@rcgt.com>
2022-02-25 05:43:07 -0800

    seed database

--- CityInformation.API/Migrations/20220225133953_dataseed.Designer.cs
+++ CityInformation.API/Migrations/20220225133953_dataseed.Designer.cs
@@ -2,6 +2,7 @@
 using CityInformation.API.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CityInformation.API.Migrations
 {
     [DbContext(typeof(CityInformationContext))]
-    partial class CityInformationContextModelSnapshot : ModelSnapshot
+    [Migration("20220225133953_dataseed")]
+    partial class dataseed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.2");
